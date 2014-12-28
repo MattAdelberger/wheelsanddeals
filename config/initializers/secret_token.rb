45e1bbb62c05169b06c49d20305b114109d97677
Matt Adelberger <matt.adelberger@gmail.com>
2014-12-28 14:16:27 -0600

    Initial Commit - Created Empty Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wheelsanddeals::Application.config.secret_key_base = '22b8f61e5426487929d376e7463fe548ff5419b4fadd3f7d354c6bcd63e2c4e1694931514e8ac2b64922725e7bd5b8d7443758875977f5893aaaedf8e411caa4'
